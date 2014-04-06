b3477bfad07b6ddad34455b5ed5203e11229b1dc
Meekgal <mpmadej@oakland.edu>
2014-04-05 21:40:46 -0400

    Home/Search Page
    So i uploaded the home and search page.
    I also added a title Image. Let me know if you want something changed...
    Or just change it... yup

---
+++ app/webroot/movieSearch.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="./js/jquery-1.9.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./css/custom.css">
+</head>
+
+<body>
+<div class="container title">
+<a href="./home.html"><center><img class="changeImg" src="./img/uflick.jpg"></center></a>
+</div>
+
+<div class="navbar navbar-inverse navbar-static-top navHeader" role="navigation">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+            </button>
+            <!--If you make this a php file please update this line-->
+		<a class="navbar-brand" href="./home.html">Home</a>
+            <!------------------------------------------------------->
+	</div>
+<div class="navbar-collapse collapse navHeader" style="height: 1px;">
+            <ul class="nav navbar-nav">
+              <!--If you make this a php file please update this line-->
+              <li><a href="./movieSearch.php">Search</a></li>
+              <!------------------------------------------------------->
+        </ul>
+</div>
+</div>
+
+<div class="container mainWrapper">
+	
+<form>
+	<center><table class="table table-bordered tableCustom">
+		<tr><td><h3><center>Movie Search</h3></center></td></tr>
+		<tr><td>Something for search</td></tr>
+		<tr><td>Something for Search</td></tr>
+		<tr><td>Something for Search</td></tr>
+		<tr><td><center><button class="btn btn-success">Submit</button></center></td></tr></center>
+		
+		<!--Display posters here-->
+	</form>
+</div>
+
+
+</body>
+</html>
